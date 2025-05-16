12ca119c21cf354070cf714642a16010db66c328
Ashish <ashishjha5463@gmail.com>
2025-05-16 16:06:43 +0530

    first

---
+++ script.js
@@ -0,0 +1,51 @@
+ 
+ 
+ const taskInput = document.getElementById('taskInput');
+ const taskList = document.getElementById('taskList');
+ 
+ 
+ function addTask(){
+    if (taskInput.value===""){
+        alert("write something in box");
+    }
+    else{
+    const newtask= document.createElement('li');
+   
+    taskList.appendChild(newtask);
+    newtask.textContent= taskInput.value;
+    let span = document.createElement("span");
+    span.innerHTML="delete";
+    newtask.appendChild(span);
+
+    savedata();
+
+    }
+    taskInput.value="";
+    
+    
+ }
+
+
+
+
+
+taskList.addEventListener("click", function(e){
+
+    if(e.target.tagName==="SPAN"){
+          e.target.parentElement.remove();
+    }
+
+}, false
+)
+
+function  savedata () {
+     localStorage.setItem("data",taskList.innerHTML)
+    }
+
+function showdata(){
+    
+    taskList.innerHTML = localStorage.getItem("data");
+
+}    
+
+showdata();
